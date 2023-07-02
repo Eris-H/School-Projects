4b6f9c56c06ddac939d03cee648dfaf72444701f
Eris H <137004932+Eris-H@users.noreply.github.com>
2023-07-02 16:24:30 -0700

    Add files via upload

--- Palindrome and User Input/UserInput.js
+++ Palindrome and User Input/UserInput.js
@@ -1,45 +1,81 @@
 //ERYX HORTIZUELA
+
 function PalinCheck()
 {
 
+    //grabs the input
     var Palin = document.getElementById("Palindrome").value;
     
+    //prepares the variable for the check
     var palinYes;
 
-        //1. split 2. reverse string 3. join string
-        var splitPal = Palin.split("");
-        //alert ("split " + splitStr);
-        var revPal = splitPal.reverse();
-        //alert ("reverse " + revStr);
-        var joinPal = revPal.join("");
-        //alert ("backwards string: " + joinStr);
-    
+    //makes it lowercase so that capitalization cannot effect whether or not its a palindrome
+    Palin = Palin.toLowerCase();
 
+    //splits, reverses, then rejoins the palendrome to invert it
+    var splitPal = Palin.split("");
+    var revPal = splitPal.reverse();
+    var joinPal = revPal.join("");
+    
+    //if input is palendrome, sets to is so that is can be inserted into the update
     if (joinPal == Palin)
     {
         palinYes = " is ";
     } else{
         palinYes = " is not ";
     }
+
+    //updates the page to tell the user if the palindrome they entered is in fact a palindrome
     document.getElementById("palinValid").innerHTML = "the word " + Palin + palinYes + " a palindrome";
 
 }
 
+//creates variable to use for name and zipcheck, cont no means zipcheck can't run
+var cont = "no";
 
 function NameCheck()
 {
 
+    //finds the names inputed
     var fNam = document.getElementById("FirstName").value;
     var lNam = document.getElementById("LastName").value;
 
+    //creates the combination of names
     var FullName = fNam + " " + lNam;
+    var ValidYes;
 
-    if (FullName > 20)
+    //checks the total name length
+    if (FullName.length > 20)
+    {
+        cont = "yes";
+        ValidYes = "VALID";
+    }
+    else 
+    {
+        ValidYes = "NOT VALID";
+    }
+
+    //shows whether or not the name inputed is valid
+    document.getElementById("nameValid").innerHTML = "Name Entered " + FullName + " is " + ValidYes;
+
+}
+
+function ZipCheck()
+{
+
+    //grabs the zip number
+    var Zips = document.getElementById("Zips").value;
+
+    //shows the secret message if the name was valid and zip was 5 digits
+    if (cont == "yes" && Zips.length == 5)
+    {
+        //i found out that mark works even if put inside the javascript file
+        document.getElementById("revealText").innerHTML = "<mark> YOU HAVE BEEN GRANTED ACCESS TO THE EMPIRES RAILWAY SYSTEM, PROCEED WITH CAUTION </mark>";
+        document.getElementById("secretText").innerHTML = " ";
+    }
+    else
     {
 
     }
-    var ValidYes = "NOT VALID";
-
-    document.getElementById("nameValid").innerHTML = "Name Entered = " + FullName + "is " + ValidYes;
 
 }
